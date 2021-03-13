4ce6d53a8ff76afe4ccf9b90910a52ce046408e1
April Castaneda <castanap@oregonstate.edu>
2021-03-13 14:41:21 -0800

    Configured firebase/location

--- lib/app.dart
+++ lib/app.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+
 class App extends StatelessWidget {
   // This widget is the root of your application.
   @override
