75590bbd59e5141d1ef6bf8d1aa52f6081965d9c
April Castaneda <castanap@oregonstate.edu>
2021-03-13 19:22:17 -0800

    Null error catching for snapshot

--- lib/screens/waste_list_screen.dart
+++ lib/screens/waste_list_screen.dart
@@ -25,7 +25,9 @@ class _WasteListScreenState extends State<WasteListScreen> {
       body: StreamBuilder(
           stream: FirebaseFirestore.instance.collection('posts').snapshots(),
           builder: (context, snapshot) {
-            if (!snapshot.hasData)
+            if (!snapshot.hasData ||
+                snapshot.data.docs == null ||
+                snapshot.data.docs.length <= 0)
               return Center(child: CircularProgressIndicator());
             return ListView.builder(
               // itemExtent: 80.0,
@@ -46,11 +48,25 @@ class _WasteListScreenState extends State<WasteListScreen> {
 
   Widget postTile(BuildContext context, DocumentSnapshot document) {
     return ListTile(
-        leading: Text(
-            DateFormat('EEEE, MMMM d, yyyy').format(document['date'].toDate())),
-        title: Text(
-          document['num_items'].toString(),
-        ),
+        leading: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                DateFormat('EEEE, MMMM d, yyyy')
+                    .format(document['date'].toDate()),
+                style: Theme.of(context).textTheme.headline6,
+              )
+            ]),
+        title: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              Text(
+                document['num_items'].toString(),
+                style: Theme.of(context).textTheme.headline6,
+              )
+            ]),
         onTap: () {});
   }
 }
